747f9e3d1de9fc3cc3df235cb459b3d9074780d4
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-01-29 14:55:49 +0600

    setup done

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const app = express();
-const port = 3000;
+dotenv.config();
+
+const port = process.env.port;
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server in Runing on port ${port}!`));
