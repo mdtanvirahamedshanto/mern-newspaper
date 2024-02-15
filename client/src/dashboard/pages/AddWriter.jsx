9b321257ad4dfcebd3cbd5ee5263d7d3b5c85991
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-15 22:24:37 +0600

    Add writer with axios in frontend

--- client/src/dashboard/pages/AddWriter.jsx
+++ client/src/dashboard/pages/AddWriter.jsx
@@ -1,6 +1,42 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { base_url } from "../../config/config";
+import StoreContext from "../../contexts/StoreContext";
 
 const AddWriter = () => {
+  const { store } = useContext(StoreContext);
+  const [state, setState] = useState({
+    name: "",
+    category: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        `${base_url}/api/news/writer/add`,
+        state,
+        {
+          headers: {
+            authorization: `Bearer ${store.token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const categors = [
     "Category",
     "Health",
@@ -35,20 +71,23 @@ const AddWriter = () => {
               <input
                 type="text"
                 placeholder="Name"
+                name="name"
                 className="px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10"
+                onChange={handleInput}
               />
             </div>
             <div className="flex flex-col gap-y-2">
               <label
-                htmlFor="name"
+                htmlFor="category"
                 className="text-md font-medium text-gray-600"
               >
                 Category
               </label>
               <select
-                name=""
+                name="category"
                 id=""
                 className="px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10"
+                onChange={handleInput}
               >
                 {categors.map((category) => (
                   <option key={category} value={category}>
@@ -59,32 +98,36 @@ const AddWriter = () => {
             </div>
             <div className="flex flex-col gap-y-2">
               <label
-                htmlFor="name"
+                htmlFor="email"
                 className="text-md font-medium text-gray-600"
               >
                 Email
               </label>
               <input
-                type="text"
-                placeholder="Email"
+                type="email"
+                placeholder="email"
+                name="email"
                 className="px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10"
+                onChange={handleInput}
               />
             </div>
             <div className="flex flex-col gap-y-2">
               <label
-                htmlFor="name"
+                htmlFor="password"
                 className="text-md font-medium text-gray-600"
               >
                 Password
               </label>
               <input
                 type="text"
-                placeholder="Password"
+                placeholder="password"
+                name="password"
                 className="px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10"
+                onChange={handleInput}
               />
             </div>
           </div>
-          <div className="mt-8 text-center">
+          <div className="mt-8 text-center" onClick={handleSubmit}>
             <Link className="px-3 py-[6px] bg-blue-500 rounded-lg text-white hover:bg-blue-900">
               Add Writer
             </Link>
