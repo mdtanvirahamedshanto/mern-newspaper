a3d86917082e5ba9982c1e0a083ddc2e5ce62f2d
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-25 23:53:32 +0600

    change

--- client/src/dashboard/pages/Writers.jsx
+++ client/src/dashboard/pages/Writers.jsx
@@ -26,6 +26,7 @@ const Writers = () => {
 
   useEffect(() => {
     getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div className="bg-white rounded-md">
